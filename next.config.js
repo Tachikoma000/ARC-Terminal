453dabc0ff70822e26523e6fc05196903d6f278e
Rin_0xTohsaka <89101179+Tachikoma000@users.noreply.github.com>
2024-12-10 03:49:05 -0600

    clean up

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/media/',
-          outputPath: 'static/media/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
-    return config;
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
